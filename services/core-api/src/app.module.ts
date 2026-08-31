2d12bcbbf5b0e3f5f3e8e417eb927531a8d3a9c9
thaminuZs <th.sonal@hotmail.com>
2026-08-31 17:33:15 +0530

    init: db mock config

--- services/core-api/src/app.module.ts
+++ services/core-api/src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OwnersModule } from './owners/owners.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [OwnersModule],
+  imports: [
+    OwnersModule,
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [],
+      useFactory: () => ({
+        type: 'postgres',
+        entities: [],
+        synchronize: true,
+        port: 5432,
+        host: 'localhost',
+        username: 'postgres',
+        password: 'toor',
+        database: 'estate360-test',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
