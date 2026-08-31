1b0d6e775dc52a735e5ddca0dbd0e6aea0a36022
thaminuZs <th.sonal@hotmail.com>
2026-08-31 20:37:28 +0530

    init: user repo db

--- services/core-api/src/owners/providers/owners.service.ts
+++ services/core-api/src/owners/providers/owners.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Owner } from '../owner.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateOwnerDto } from '../dtos/createOwnerDto';
 
 @Injectable()
-export class OwnersService {}
+export class OwnersService {
+  constructor(
+    @InjectRepository(Owner)
+    private readonly ownersRepository: Repository<Owner>,
+  ) {}
+
+  public async createOwner(createOwnerDto: CreateOwnerDto) {
+    let owner = this.ownersRepository.create(createOwnerDto);
+    owner = await this.ownersRepository.save(owner);
+
+    return owner;
+  }
+}
